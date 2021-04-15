f1e0a9bbd398b99c96496b4e7fd9171a0f2f831a
Captain9965 <lennyweda@gmail.com>
2021-04-15 16:49:55 +0300

    Added login function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
+@app.route('/', methods=['GET'])             # Home route
 @app.route('/index')
 def index():
     user={'username': 'Lenny'}  # mock object
@@ -17,6 +18,14 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET','POST'])    # Login route
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))         # display message to the user
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 

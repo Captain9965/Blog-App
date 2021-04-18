28e52af94edf6c089e55e6034423e13194077e22
Captain9965 <lennyweda@gmail.com>
2021-04-18 14:05:11 +0300

    Added posts table

--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
+from datetime import datetime
 from app import db
+
 
 class User(db.Model):   # inherits from db.Model, a base class for all models from Flask-SQLAlchemy.
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self): # tells python  how to print objects of this class, which is going to be useful for debugging.   
         return '<User {}>'.format(self.username)    
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)#When you pass a function as a default, SQLAlchemy will set the field to the value of calling that function 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))#references an id value from the users table.
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)

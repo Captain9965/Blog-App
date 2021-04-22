4d4a98295294e551450649004a7a042fc45a27b1
Captain9965 <lennyweda@gmail.com>
2021-04-22 16:25:06 +0300

    add cuatom error messages

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,5 @@ db=SQLAlchemy(app)      # create database instance
 migrate= Migrate(app, db)# database migration engine instance
 login=LoginManager(app) #initialize login instance
 login.login_view = 'login' #for flask_login to know the view that handles login
-from app import routes, models  # importing the routes module and models module that will define the database structure
+from app import routes, models, errors  # importing the routes module and models module that will define the database structure
                         # The bottom import is a workaround to circular imports, a common problem with Flask applications

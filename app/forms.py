db021be9135bb802c35212c2363fe3d3461a82d9
Captain9965 <lennyweda@gmail.com>
2021-05-01 14:39:31 +0300

    Added followers

--- app/forms.py
+++ app/forms.py
@@ -39,3 +39,5 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from app import db, login
 from flask_login import UserMixin
 from hashlib import md5 #for avatars
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 @login.user_loader  #The user loader is registered with Flask-Login with the @login.user_loader decorator. 
 def load_user(id):
@@ -17,6 +21,11 @@ class User(UserMixin, db.Model):   # inherits from db.Model, a base class for al
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):     #tells python  how to print objects of this class, which is going to be useful for debugging.   
         return '<User {}>'.format(self.username)    
@@ -29,6 +38,23 @@ class User(UserMixin, db.Model):   # inherits from db.Model, a base class for al
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+    def followed_posts(self):#create temporary table by join method according to condition, then filter, then order
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())  #union operator
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -37,3 +63,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
